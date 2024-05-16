f67d304f5536666a7653319a86980e1dd0671c0a
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-05-16 15:42:45 -0400

    [fix] index

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const PORT = process.env.PORT ?? 3000
+
+app.use(express.json());
+app.use(cors());
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`)
+})
